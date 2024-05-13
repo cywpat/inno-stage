36083c998603baa7fb484aaac3f228f58c262dd5
cs <54385174+csndylim@users.noreply.github.com>
2024-05-13 18:20:03 +0800

    Squashed commit of the following:
    commit 23b1cac5599c3bfdd8799b1add137cc27499b55b
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Mon May 13 18:18:08 2024 +0800
    
        move the login components to another folder
    
    commit 314a125fd02675c9dd566097dda68565f85cc726
    Merge: 2ce4f4d4 771081df
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Mon May 13 18:14:26 2024 +0800
    
        Merge remote-tracking branch 'origin/login-signup' into candy
    
    commit 2ce4f4d41edc40b5b2808e2a1491e9d94217a0ed
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Mon May 13 18:11:56 2024 +0800
    
        Update package-lock.json
    
    commit ad825eed31c6fae3739f8a9b71df83f97720bace
    Merge: 31543d34 29100252
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Mon May 13 18:11:40 2024 +0800
    
        Merge branch 'master' into candy
    
    commit 31543d34c02051bbc356d0d9ef3f1ed040ca1a8a
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Mon May 13 17:24:09 2024 +0800
    
        Database Script, REST api with backend and frontend, populate staging data in Engineers.tsx
    
        Database Script
        - Create 3 tables: Engineers, Staging Status, Combined eDSR + eTSR
    
        REST API
        - added proxy on package.json in frontend
        - added CORS allowed orgins on settings.py in backend
    
        staging data
        - backend: models.py, views.py, urls.py
        - frontend:  Engineer.tsx
    
    commit 771081df072a68256b4c39b7d1511fa6cf7775ca
    Author: TohJm <126372268+TohJm@users.noreply.github.com>
    Date:   Sun May 12 20:53:44 2024 +0800
    
        login-signup v1
    
    commit d23191aae71896d0b28870e5cc378cab42c80125
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Fri May 10 15:57:14 2024 +0800
    
        move files to frontend folder
    
    commit 408befbb92ffb0f5c3214302a3d169f070ea3802
    Author: cs <54385174+csndylim@users.noreply.github.com>
    Date:   Fri May 10 14:59:54 2024 +0800
    
        react changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/login.css';
 
-function Login() {
+import user_icon from '../images/person.png';
+import email_icon from '../images/email.png';
+import password_icon from '../images/password.png';
+
+const Login = () => {
+    const [action, setAction] = useState("Sign Up");
+    
     return (
-        <div>Login</div>    
-    );
+        <div className="login-page">
+                <div className="header">
+                    <div className="text">Sign up</div>
+                    <div className="underline"></div>
+                </div>
+
+                <div className="inputs">
+                    {action === "Login" ? <div></div> : <div className="input">
+                        <img src={user_icon} alt=" " />
+                        <input type="text" placeholder="Name" />
+                    </div>}
+
+                    <div className="input">
+                        <img src={email_icon} alt=" " />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input">
+                        <img src={password_icon} alt=" " />
+                        <input type="password" placeholder="Password" />
+                    </div>
+                </div>
+                <div className="forget-password">Lost Password? <span>Click Here to Reset</span></div>
+                <div className="submit-container">
+                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign Up</div>
+                    <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</div>
+                </div>
+        </div>
+    )
 }
 
 export default Login;
